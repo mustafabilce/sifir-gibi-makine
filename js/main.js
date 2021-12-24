e2764620ac393e327adac6b6e710299da886ec34
mustafabilce <52863120+mustafabilce@users.noreply.github.com>
2021-12-24 13:26:59 +0300

    deploy test

---
+++ js/main.js
@@ -0,0 +1,75 @@
+$("#kategori-slider").owlCarousel({
+  loop: true,
+  autoplay: false,
+  margin: 20,
+  nav: true,
+  dots: false,
+  responsive: {
+    0: {
+      items: 3,
+    },
+  },
+});
+
+$("#yeni-eklenenler-slider").owlCarousel({
+  loop: true,
+  autoplay: false,
+  margin: 20,
+  nav: true,
+  dots: false,
+  responsive: {
+    0: {
+      items: 3,
+    },
+  },
+});
+
+$("#ihalelerimiz-slider").owlCarousel({
+  loop: true,
+  autoplay: false,
+  margin: 20,
+  nav: true,
+  dots: false,
+  responsive: {
+    0: {
+      items: 3,
+    },
+  },
+});
+
+$("#blog-slider").owlCarousel({
+  loop: true,
+  autoplay: false,
+  margin: 20,
+  nav: true,
+  dots: false,
+  responsive: {
+    0: {
+      items: 3,
+    },
+  },
+});
+
+$("#urun-detay-slider").owlCarousel({
+  items:1,
+  dots: false,
+  loop:false,
+  center:true,
+  margin:10,
+  URLhashListener:true,
+  autoplayHoverPause:true,
+  startPosition: 'URLHash'
+});
+
+$("#benzer-urunler-slider").owlCarousel({
+  loop: true,
+  autoplay: false,
+  margin: 20,
+  nav: true,
+  dots: false,
+  responsive: {
+    0: {
+      items: 4,
+    },
+  },
+});
