4db03715f9be0a62e8d3f4d8adb1c2ff61a852eb
mustafabilce <52863120+mustafabilce@users.noreply.github.com>
2021-12-29 18:34:52 +0300

    links page and other pages created.

--- js/main.js
+++ js/main.js
@@ -51,14 +51,14 @@ $("#blog-slider").owlCarousel({
 });
 
 $("#urun-detay-slider").owlCarousel({
-  items:1,
+  items: 1,
   dots: false,
-  loop:false,
-  center:true,
-  margin:10,
-  URLhashListener:true,
-  autoplayHoverPause:true,
-  startPosition: 'URLHash'
+  loop: false,
+  center: true,
+  margin: 10,
+  URLhashListener: true,
+  autoplayHoverPause: true,
+  startPosition: "URLHash",
 });
 
 $("#benzer-urunler-slider").owlCarousel({
@@ -73,3 +73,30 @@ $("#benzer-urunler-slider").owlCarousel({
     },
   },
 });
+
+/* COUNTER */
+
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEL = document.getElementById("mins");
+const secondsEL = document.getElementById("seconds");
+
+const newYears = "30 December 2022";
+
+function countdown() {
+  const newYearsDate = new Date(newYears);
+  const currentDate = new Date();
+
+  const totalSeconds = (newYearsDate - currentDate) / 1000;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const seconds = Math.floor(totalSeconds) % 60;
+
+  daysEl.innerText = days;
+  hoursEl.innerText = hours;
+  minsEL.innerText = minutes;
+  secondsEL.innerText = seconds;
+}
+
+setInterval(countdown, 1000);
