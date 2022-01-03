5e332cc970a185d09a1e122e508d7983ab3c0546
mustafabilce <52863120+mustafabilce@users.noreply.github.com>
2022-01-03 17:03:22 +0300

    counters revised

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 $("#banner-slider").owlCarousel({
-  items:1,
-  loop:false,
-  center:true,
-  margin:10,
+  items: 1,
+  loop: false,
+  center: true,
+  margin: 10,
   dots: false,
-  URLhashListener:true,
-  autoplayHoverPause:true,
-  startPosition: 'URLHash'
+  URLhashListener: true,
+  autoplayHoverPause: true,
+  startPosition: "URLHash",
 });
 
 $("#kategori-slider").owlCarousel({
@@ -111,30 +111,264 @@ $("#benzer-urunler-slider").owlCarousel({
 
 /* COUNTER */
 
-const daysEl = document.getElementById("days");
-const hoursEl = document.getElementById("hours");
-const minsEL = document.getElementById("mins");
-const secondsEL = document.getElementById("seconds");
+// Create Countdown
+var Countdown = {
+  // Backbone-like structure
+  $el: $(".countdown"),
 
-const newYears = "30 December 2022";
+  // Params
+  countdown_interval: null,
+  total_seconds: 0,
 
-function countdown() {
-  const newYearsDate = new Date(newYears);
-  const currentDate = new Date();
+  // Initialize the countdown
+  init: function () {
+    // DOM
+    this.$ = {
+      hours: this.$el.find(".bloc-time.hours .figure"),
+      minutes: this.$el.find(".bloc-time.min .figure"),
+      seconds: this.$el.find(".bloc-time.sec .figure"),
+    };
 
-  const totalSeconds = (newYearsDate - currentDate) / 1000;
-  const minutes = Math.floor(totalSeconds / 60) % 60;
-  const hours = Math.floor(totalSeconds / 3600) % 24;
-  const days = Math.floor(totalSeconds / 3600 / 24);
-  const seconds = Math.floor(totalSeconds) % 60;
+    // Init countdown values
+    this.values = {
+      hours: this.$.hours.parent().attr("data-init-value"),
+      minutes: this.$.minutes.parent().attr("data-init-value"),
+      seconds: this.$.seconds.parent().attr("data-init-value"),
+    };
 
-  daysEl.innerText = days;
-  hoursEl.innerText = hours;
-  minsEL.innerText = minutes;
-  secondsEL.innerText = seconds;
-}
+    // Initialize total seconds
+    this.total_seconds =
+      this.values.hours * 60 * 60 +
+      this.values.minutes * 60 +
+      this.values.seconds;
 
-setInterval(countdown, 1000);
+    // Animate countdown to the end
+    this.count();
+  },
+
+  count: function () {
+    var that = this,
+      $hour_1 = this.$.hours.eq(0),
+      $hour_2 = this.$.hours.eq(1),
+      $min_1 = this.$.minutes.eq(0),
+      $min_2 = this.$.minutes.eq(1),
+      $sec_1 = this.$.seconds.eq(0),
+      $sec_2 = this.$.seconds.eq(1);
+
+    this.countdown_interval = setInterval(function () {
+      if (that.total_seconds > 0) {
+        --that.values.seconds;
+
+        if (that.values.minutes >= 0 && that.values.seconds < 0) {
+          that.values.seconds = 59;
+          --that.values.minutes;
+        }
+
+        if (that.values.hours >= 0 && that.values.minutes < 0) {
+          that.values.minutes = 59;
+          --that.values.hours;
+        }
+
+        // Update DOM values
+        // Hours
+        that.checkHour(that.values.hours, $hour_1, $hour_2);
+
+        // Minutes
+        that.checkHour(that.values.minutes, $min_1, $min_2);
+
+        // Seconds
+        that.checkHour(that.values.seconds, $sec_1, $sec_2);
+
+        --that.total_seconds;
+      } else {
+        clearInterval(that.countdown_interval);
+      }
+    }, 1000);
+  },
+
+  animateFigure: function ($el, value) {
+    var that = this,
+      $top = $el.find(".top"),
+      $bottom = $el.find(".bottom"),
+      $back_top = $el.find(".top-back"),
+      $back_bottom = $el.find(".bottom-back");
+
+    // Before we begin, change the back value
+    $back_top.find("span").html(value);
+
+    // Also change the back bottom value
+    $back_bottom.find("span").html(value);
+
+    // Then animate
+    TweenMax.to($top, 0.8, {
+      rotationX: "-180deg",
+      transformPerspective: 300,
+      ease: Quart.easeOut,
+      onComplete: function () {
+        $top.html(value);
+
+        $bottom.html(value);
+
+        TweenMax.set($top, { rotationX: 0 });
+      },
+    });
+
+    TweenMax.to($back_top, 0.8, {
+      rotationX: 0,
+      transformPerspective: 300,
+      ease: Quart.easeOut,
+      clearProps: "all",
+    });
+  },
+
+  checkHour: function (value, $el_1, $el_2) {
+    var val_1 = value.toString().charAt(0),
+      val_2 = value.toString().charAt(1),
+      fig_1_value = $el_1.find(".top").html(),
+      fig_2_value = $el_2.find(".top").html();
+
+    if (value >= 10) {
+      // Animate only if the figure has changed
+      if (fig_1_value !== val_1) this.animateFigure($el_1, val_1);
+      if (fig_2_value !== val_2) this.animateFigure($el_2, val_2);
+    } else {
+      // If we are under 10, replace first figure with 0
+      if (fig_1_value !== "0") this.animateFigure($el_1, 0);
+      if (fig_2_value !== val_1) this.animateFigure($el_2, val_1);
+    }
+  },
+};
+
+Countdown.init();
+
+var Countdown = {
+  // Backbone-like structure
+  $el: $(".countdown2"),
+
+  // Params
+  countdown_interval: null,
+  total_seconds: 0,
+
+  // Initialize the countdown
+  init: function () {
+    // DOM
+    this.$ = {
+      hours: this.$el.find(".bloc-time.hours .figure"),
+      minutes: this.$el.find(".bloc-time.min .figure"),
+      seconds: this.$el.find(".bloc-time.sec .figure"),
+    };
+
+    // Init countdown values
+    this.values = {
+      hours: this.$.hours.parent().attr("data-init-value"),
+      minutes: this.$.minutes.parent().attr("data-init-value"),
+      seconds: this.$.seconds.parent().attr("data-init-value"),
+    };
+
+    // Initialize total seconds
+    this.total_seconds =
+      this.values.hours * 60 * 60 +
+      this.values.minutes * 60 +
+      this.values.seconds;
+
+    // Animate countdown to the end
+    this.count();
+  },
+
+  count: function () {
+    var that = this,
+      $hour_1 = this.$.hours.eq(0),
+      $hour_2 = this.$.hours.eq(1),
+      $min_1 = this.$.minutes.eq(0),
+      $min_2 = this.$.minutes.eq(1),
+      $sec_1 = this.$.seconds.eq(0),
+      $sec_2 = this.$.seconds.eq(1);
+
+    this.countdown_interval = setInterval(function () {
+      if (that.total_seconds > 0) {
+        --that.values.seconds;
+
+        if (that.values.minutes >= 0 && that.values.seconds < 0) {
+          that.values.seconds = 59;
+          --that.values.minutes;
+        }
+
+        if (that.values.hours >= 0 && that.values.minutes < 0) {
+          that.values.minutes = 59;
+          --that.values.hours;
+        }
+
+        // Update DOM values
+        // Hours
+        that.checkHour(that.values.hours, $hour_1, $hour_2);
+
+        // Minutes
+        that.checkHour(that.values.minutes, $min_1, $min_2);
+
+        // Seconds
+        that.checkHour(that.values.seconds, $sec_1, $sec_2);
+
+        --that.total_seconds;
+      } else {
+        clearInterval(that.countdown_interval);
+      }
+    }, 1000);
+  },
+
+  animateFigure: function ($el, value) {
+    var that = this,
+      $top = $el.find(".top"),
+      $bottom = $el.find(".bottom"),
+      $back_top = $el.find(".top-back"),
+      $back_bottom = $el.find(".bottom-back");
+
+    // Before we begin, change the back value
+    $back_top.find("span").html(value);
+
+    // Also change the back bottom value
+    $back_bottom.find("span").html(value);
+
+    // Then animate
+    TweenMax.to($top, 0.8, {
+      rotationX: "-180deg",
+      transformPerspective: 300,
+      ease: Quart.easeOut,
+      onComplete: function () {
+        $top.html(value);
+
+        $bottom.html(value);
+
+        TweenMax.set($top, { rotationX: 0 });
+      },
+    });
+
+    TweenMax.to($back_top, 0.8, {
+      rotationX: 0,
+      transformPerspective: 300,
+      ease: Quart.easeOut,
+      clearProps: "all",
+    });
+  },
+
+  checkHour: function (value, $el_1, $el_2) {
+    var val_1 = value.toString().charAt(0),
+      val_2 = value.toString().charAt(1),
+      fig_1_value = $el_1.find(".top").html(),
+      fig_2_value = $el_2.find(".top").html();
+
+    if (value >= 10) {
+      // Animate only if the figure has changed
+      if (fig_1_value !== val_1) this.animateFigure($el_1, val_1);
+      if (fig_2_value !== val_2) this.animateFigure($el_2, val_2);
+    } else {
+      // If we are under 10, replace first figure with 0
+      if (fig_1_value !== "0") this.animateFigure($el_1, 0);
+      if (fig_2_value !== val_1) this.animateFigure($el_2, val_1);
+    }
+  },
+};
+
+Countdown.init();
 
 /* Flip Card */
 
@@ -172,32 +406,32 @@ $(".counting").each(function () {
   );
 });
 
-$( "#slider-item-1-right" ).click(function() {
-  $( "#slider-item-1-right" ).toggleClass( "d-none" );
-  $( "#slider-item-1-left" ).toggleClass( "d-block" );
+$("#slider-item-1-right").click(function () {
+  $("#slider-item-1-right").toggleClass("d-none");
+  $("#slider-item-1-left").toggleClass("d-block");
 });
 
-$( "#slider-item-1-left" ).click(function() {
-  $( "#slider-item-1-right" ).toggleClass( "d-block" );
-  $( "#slider-item-1-left" ).removeClass( "d-block" );
+$("#slider-item-1-left").click(function () {
+  $("#slider-item-1-right").toggleClass("d-block");
+  $("#slider-item-1-left").removeClass("d-block");
 });
 
-$( "#slider-item-2-right" ).click(function() {
-  $( "#slider-item-2-right" ).toggleClass( "d-none" );
-  $( "#slider-item-2-left" ).toggleClass( "d-block" );
+$("#slider-item-2-right").click(function () {
+  $("#slider-item-2-right").toggleClass("d-none");
+  $("#slider-item-2-left").toggleClass("d-block");
 });
 
-$( "#slider-item-2-left" ).click(function() {
-  $( "#slider-item-2-right" ).toggleClass( "d-block" );
-  $( "#slider-item-2-left" ).removeClass( "d-block" );
+$("#slider-item-2-left").click(function () {
+  $("#slider-item-2-right").toggleClass("d-block");
+  $("#slider-item-2-left").removeClass("d-block");
 });
 
-$( "#slider-item-3-right" ).click(function() {
-  $( "#slider-item-3-right" ).toggleClass( "d-none" );
-  $( "#slider-item-3-left" ).toggleClass( "d-block" );
+$("#slider-item-3-right").click(function () {
+  $("#slider-item-3-right").toggleClass("d-none");
+  $("#slider-item-3-left").toggleClass("d-block");
 });
 
-$( "#slider-item-3-left" ).click(function() {
-  $( "#slider-item-3-right" ).toggleClass( "d-block" );
-  $( "#slider-item-3-left" ).removeClass( "d-block" );
+$("#slider-item-3-left").click(function () {
+  $("#slider-item-3-right").toggleClass("d-block");
+  $("#slider-item-3-left").removeClass("d-block");
 });
